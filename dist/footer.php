0bb3a5fc36fc7842b20c8753011624afb1a44e91
Kirby <mshhll.rk@gmail.com>
2020-04-10 13:14:43 -0700

    First commit

---
+++ dist/footer.php
@@ -0,0 +1,8 @@
+        </body>
+        <footer>
+        </footer>
+        <script src="js/jquery.v3.4.1.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="js/jquery.fitvids.js"></script>
+        <script src="js/scripts.js"></script>
+</html>
---
+++ dist/modules/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Page Title</title>
+  </head>
+  <header>
+  </header>
