6903d9ca17886dc54ee5f44d6e047b4c4710f476
Kirby <mshhll.rk@gmail.com>
2020-06-04 12:00:00 -0700

    Initial Commit

---
+++ dist/js/scripts.js
@@ -0,0 +1,4 @@
+jQuery(document).ready(function( $ ) { 
+        $('.video').fitVids({ customSelector: "iframe[src^='https://drive.google.com/file/d/1j3SPcu9vqB0AtA5cug5AjnWLVEOzSvO8/preview']"});
+
+});
