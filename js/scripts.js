8215cfe697518253982f8780ba86e0a29bd806e8
Kirby <mshhll.rk@gmail.com>
2020-06-05 08:49:37 -0700

    Dekstop design progress - mobile is J A N K E E E

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
 jQuery(document).ready(function( $ ) { 
-        $('.video').fitVids({ customSelector: "iframe[src^='https://drive.google.com/file/d/1j3SPcu9vqB0AtA5cug5AjnWLVEOzSvO8/preview']"});
+        $('.video').fitVids({ });
 
+        $(window).scroll(function() {
+                if ($(document).scrollTop() > 50) {
+                        $('.header').addClass('shrink');
+                        console.log("scroll");
+                } else {
+                $('.header').removeClass('shrink');
+          }
+        });
 });
